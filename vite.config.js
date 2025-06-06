c8d298feff58251b2e9981115f9794107edd4670
Dante <Dante@anchor-homes.com>
2025-06-06 03:42:23 +0000

    0606 GCP同步

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173, // 默认端口，您可以根据需要修改
+  },
 })
